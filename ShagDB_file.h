ce17a84480b030676f19d06c213d0063df6c0750
Mikhailshag <45270605+Mikhailshag@users.noreply.github.com>
2019-02-14 17:56:56 +0300

    Update ShagDB_file.h

--- ShagDB_file.h
+++ ShagDB_file.h
@@ -4,10 +4,14 @@
 
 using namespace std;
 
+int pos;
+int idNUM;
+float middle_acc;
+
 string record = "record.txt";
 string points = "points.txt";
-string buffer,str;
-int semicolon = 0,k,i=0;
+string buffer, str1, str2, str3, str4, str5, str6, str7, str8;
+string readSTR1, readSTR2, readSTR3, readSTR4, readSTR5, readSTR6, readSTR7, readSTR8;
 
 void StudentOutputFile(Student *stud)
 {
@@ -16,11 +20,11 @@ void StudentOutputFile(Student *stud)
 
 	if (!file.is_open())
 	{
-		cout << "\nÔàéë íå îòêðûò\n\n" << endl;
+		cout << "\nÐ¤Ð°Ð¹Ð» Ð½Ðµ Ð¾Ñ‚ÐºÑ€Ñ‹Ñ‚\n\n" << endl;
 	}
 	else
 	{
-		cout << "\nÔàéë  îòêðûò\n\n" << endl;
+		cout << "\nÐ¤Ð°Ð¹Ð»  Ð¾Ñ‚ÐºÑ€Ñ‹Ñ‚\n\n" << endl;
 
 		file << stud->id << ";" << stud->people.family << ";" << stud->people.name << ";" << stud->people.contact.address << ";" << stud->people.contact.tel << ";" << stud->middle_ac << ";" << stud->facult.group << ";" << stud->facult.actual << endl;
 	}
@@ -35,11 +39,11 @@ void StudentPointsOutputFile(Student *stud) {
 
 	if (!filePoints.is_open())
 	{
-		cout << "\nÔàéë íå îòêðûò\n\n" << endl;
+		cout << "\nÐ¤Ð°Ð¹Ð» Ð½Ðµ Ð¾Ñ‚ÐºÑ€Ñ‹Ñ‚\n\n" << endl;
 	}
 	else
 	{
-		cout << "\nÔàéë  îòêðûò\n\n" << endl;
+		cout << "\nâ€˜Ð°Ð¹Ð»  Ð¾Ñ‚ÐºÑ€Ñ‹Ñ‚\n\n" << endl;
 
 		filePoints << "ID = " << stud->id << ";" << endl;
 
@@ -94,37 +98,76 @@ void StudentPointsOutputFile(Student *stud) {
 
 void StudentInputFile(Student *stud) {
 
-	ifstream fileRead(record);
-	if (fileRead.is_open())
-	{
-		cout << "Ôàéë îòêðûò!\n";
-		while (!fileRead.eof())
-		{
-			getline(fileRead, buffer);
-			
-			while (buffer[k+i] != ';') {
+	
 
-				str = str + buffer[k];
-				i++;
-				break;
-			}
-		}
-		fileRead.close();
+	ifstream fileRead(record);
+	if (!fileRead.is_open())
+	{
+		cout << "\nâ€˜Ð°Ð¹Ð» Ð½Ðµ Ð¾Ñ‚ÐºÑ€Ñ‹Ñ‚\n\n";
+
 	}
 	else {
 
-		cout << "\nÔàéë íå îòêðûò\n\n"; 
+		getline(fileRead, buffer);
+		pos = buffer.find(";");
+		readSTR1 = buffer.substr(0, pos);
+		str1 = buffer.substr(pos + 1);
+
+		pos = str1.find(";");
+		readSTR2 = str1.substr(0, pos);
+		str2 = str1.substr(pos + 1);
+
+		pos = str2.find(";");
+		readSTR3 = str2.substr(0, pos);
+		str3 = str2.substr(pos + 1);
+
+		pos = str3.find(";");
+		readSTR4 = str3.substr(0, pos);
+		str4 = str3.substr(pos + 1);
+
+		pos = str4.find(";");
+		readSTR5 = str4.substr(0, pos);
+		str5 = str4.substr(pos + 1);
+
+		pos = str5.find(";");
+		readSTR6 = str5.substr(0, pos);
+		str6 = str5.substr(pos + 1);
+
+		pos = str6.find(";");
+		readSTR7 = str6.substr(0, pos);
+		str7 = str6.substr(pos + 1);
+
+		pos = str7.find(";");
+		readSTR8 = str7.substr(0, pos);
+		str8 = str7.substr(pos + 1);
+
 
 	}
+	fileRead.close();
 
+	cout << readSTR1 << " ; " << readSTR2 << " ; " << readSTR3 << " ; " << readSTR4 << " ; " << readSTR5 << " ; " << readSTR6 << " ; " << readSTR7 << endl;
 
+	idNUM = atoi(readSTR1.c_str());
+	stud->id = idNUM;
+	stud->people.family = readSTR2;
+	stud->people.name = readSTR3;
+	stud->people.contact.address = readSTR4;
+	stud->people.contact.tel = readSTR5;
+	middle_acc = atof(readSTR6.c_str());
+	stud->middle_ac = middle_acc;
+	stud->facult.group = readSTR7;
 
-	
+	if (readSTR8 == "0")
+	{
+		stud->facult.actual = false;
+	}
+	else
+	{
+		stud->facult.actual = true;
+	}
 
 	cout << endl;
-
 }
-
 
 
 #endif
